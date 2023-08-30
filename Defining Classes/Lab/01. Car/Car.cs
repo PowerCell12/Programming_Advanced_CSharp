b6245656e1c12645cc71278e6489b8d0eeb660c1
Ivan <87208452+PowerCell12@users.noreply.github.com>
2023-08-30 12:31:43 +0300

    Add files via upload

---
+++ Defining Classes/Lab/01. Car/Car.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CarManufacturer
+{
+ class Car
+ {
+ 
+    private string make;
+    private string model;
+    private int year;
+
+    public string Model {get; set;}
+    public string  Make {get; set;}
+    public int Year {get; set;}
+ 
+ 
+ }
+}
