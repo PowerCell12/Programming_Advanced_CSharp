3e5bd7d248574a68b1633e9946bdde7ec2dccb6c
Ivan <87208452+PowerCell12@users.noreply.github.com>
2023-09-05 19:08:13 +0300

    Add files via upload

---
+++ Defining Classes/Exercise/01. Define a Class Person/ Define_a_Class_Person.cs
@@ -0,0 +1,17 @@
+using System.Runtime.InteropServices;
+
+namespace DefiningClasses{
+
+    public class StartUp{
+
+        public static void Main(string[] args){
+
+            Person Josh = new(){
+                Name = "Josh",
+                Age = 30
+            };
+
+        }
+
+    }
+}
