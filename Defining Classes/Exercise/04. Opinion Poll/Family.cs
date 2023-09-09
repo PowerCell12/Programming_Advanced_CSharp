ca4964e8d1e04aaab3a31b1c3253731204210554
Ivan <87208452+PowerCell12@users.noreply.github.com>
2023-09-09 09:24:11 +0300

    Add files via upload

---
+++ Defining Classes/Exercise/04. Opinion Poll/Family.cs
@@ -0,0 +1,34 @@
+﻿using DefiningClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public List<Person> members {set; get;}
+
+        public Family(){
+            this.members = new List<Person>();
+        }
+
+        public void AddMember(Person member)
+        {
+            members.Add(member);
+        }
+
+
+        public Person GetOldestMember()
+        {
+            return members.MaxBy(x => x.Age);
+        }
+
+
+
+
+
+    }
+}
