e9623b2701222924f2487baf25a42a49757e07ad
Ivan <87208452+PowerCell12@users.noreply.github.com>
2023-09-11 09:46:12 +0300

    Add files via upload

---
+++ Defining Classes/Exercise/06. Speed Racing/Printing.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final;
+
+public static class Printing
+{
+
+   public static void Main(string[] args)
+    {
+
+        int lines = int.Parse(Console.ReadLine());
+        List<Car> all_cars = new List<Car>();
+
+
+        for (int i = 0; i < lines; i++)
+        {
+
+            List<string> commands = Console.ReadLine().Split(' ').ToList();
+
+            Car car = new Car(commands[0], double.Parse(commands[1]), double.Parse(commands[2]));
+
+            all_cars.Add(car);
+
+
+        }
+
+        while (true)
+        {
+            List<string> commands = Console.ReadLine().Split().ToList();
+
+            if (commands[0] == "End")
+            {
+                break;
+            }
+
+            foreach(var car in all_cars)
+            {
+                if (car.Model == commands[1])
+                {
+                    car.Drive(double.Parse(commands[2]));
+                }
+            }
+        }
+
+
+        foreach(var car in all_cars)
+        {
+            Console.WriteLine($"{car.Model} {car.FuelAmount:F2} {car.Travelleddistance}");
+        }
+
+
+
+    }
+
+
+
+}
+
