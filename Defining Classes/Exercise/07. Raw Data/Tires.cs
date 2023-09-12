65981af937bb3875f38a38d960e1fe7f391bb47e
Ivan <87208452+PowerCell12@users.noreply.github.com>
2023-09-12 10:34:11 +0300

    Add files via upload

---
+++ Defining Classes/Exercise/07. Raw Data/Tires.cs
@@ -0,0 +1,13 @@
+namespace Final;
+
+public class Tire{
+
+    public int age {get; set;}
+    public double pressure  {get; set;}
+
+    public Tire(double Pressure, int Age){
+        pressure = Pressure;
+        age = Age;
+
+    }
+}
