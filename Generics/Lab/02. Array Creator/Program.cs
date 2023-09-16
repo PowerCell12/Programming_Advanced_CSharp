b6232cac061cd625d7793c1a4a6768e14b005d4e
Ivan <87208452+PowerCell12@users.noreply.github.com>
2023-09-16 11:46:44 +0300

    Add files via upload

---
+++ Generics/Lab/02. Array Creator/Program.cs
@@ -0,0 +1,11 @@
+namespace GenericArrayCreator;
+
+public class StartUp{
+
+    public static void Main(string[] args){
+        
+        string[] strings = ArrayCreator.Create(5, "Pesho");
+        int[] integers = ArrayCreator.Create(10, 33);
+    }
+
+}
